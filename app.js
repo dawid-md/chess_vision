89e62e3474796b80129e86e53329362a90211924
Dawid Mądrzyk <maddawid@gmail.com>
2022-06-03 00:43:09 +0200

    zmiana id pól na szachowa adnotacje

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 function drawChessboard() {
-    let v = 0       //zmienna pomocnicza do kolorowania pól
+    let v = 0                       //zmienna pomocnicza do kolorowania pól
+    let squareLetter = 'A'
+    let squareNumber = 8
     for(let i=1; i<=64; i++) {
         let square = document.createElement('div')
-        square.id = i
+        square.id = squareLetter + squareNumber
         if ((i+v)%2 == 0) {
             square.classList.add('squareE')
         } 
@@ -14,7 +16,13 @@ function drawChessboard() {
             setTarget()
         })
         document.querySelector('#chessboard').appendChild(square)
-        i%8 == 0 ? v=v+1 : v=v      //zmiana v na +1 aby od nowego wiersza był inny kolor
+
+        squareLetter = String.fromCharCode(squareLetter.charCodeAt() + 1)   
+        if(i%8 === 0) {
+            v=v+1                   //zmiana v na +1 aby od nowego wiersza był inny kolor
+            squareLetter = 'A'      //zmiana na A od nowego wiersza
+            squareNumber -= 1       //dekrementacja, numeracja wierszy szachownicy od góry do dołu
+        }
     }
 }
 
