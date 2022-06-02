b641d60c9124b795c76916d09635343990633509
Dawid Mądrzyk <maddawid@gmail.com>
2022-06-03 01:21:23 +0200

    dodanie pelnych coordinates i setTarget po kliknieciu w pole

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 function drawChessboard() {
+    let squares = {}                //coordinates!!!
     let v = 0                       //zmienna pomocnicza do kolorowania pól
     let squareLetter = 'A'
     let squareNumber = 8
     for(let i=1; i<=64; i++) {
         let square = document.createElement('div')
-        square.id = squareLetter + squareNumber
+        square.id = i
         if ((i+v)%2 == 0) {
             square.classList.add('squareE')
         } 
@@ -12,23 +13,30 @@ function drawChessboard() {
             square.classList.add('squareO')
         }
         square.addEventListener('click', (e) => {
-            console.log(e.target.id)
-            setTarget()
+            //console.log(e.target.id)
+            setTarget(squares)
         })
         document.querySelector('#chessboard').appendChild(square)
 
-        squareLetter = String.fromCharCode(squareLetter.charCodeAt() + 1)   
-        if(i%8 === 0) {
+        squares[i] = squareLetter + squareNumber                        //dodanie id - nazwa pola do obiektu
+        squareLetter = String.fromCharCode(squareLetter.charCodeAt() + 1)   //zmiana litery na nastepna dla kolejnego pola
+        if(i%8 === 0) {             //jesli caly jeden wiersz wypelniony...
             v=v+1                   //zmiana v na +1 aby od nowego wiersza był inny kolor
             squareLetter = 'A'      //zmiana na A od nowego wiersza
             squareNumber -= 1       //dekrementacja, numeracja wierszy szachownicy od góry do dołu
         }
     }
+    return squares
 }
 
-function setTarget() {
-    document.querySelector('#targetLabel').textContent = Math.floor(Math.random() *63 +1)
+function setTarget(squares) {
+    document.querySelector('#targetLabel').textContent = squares[Math.floor(Math.random() *64 +1)]
 }
 
-drawChessboard()
-setTarget()
+const coordinates = drawChessboard()
+setTarget(coordinates)
+
+//for(j = 1; j<=500; j++) {
+//    let f = coordinates[Math.floor(Math.random() *64 +1)]
+//    if (f == 'H1') {console.log(f)}
+//}
